bf82d4b189491cfae4a1946324b33986b15a2994
shevernitskiy <mahahuha@ya.ru>
2023-11-13 14:08:47 +0300

    dict automation prototype

---
+++ automation/dict_manifest_update.py
@@ -0,0 +1,19 @@
+import binascii
+import json
+from pathlib import Path
+
+import requests
+
+file = Path("./metadata/dict.json")
+
+manifest = json.loads(file.read_text(encoding="utf-8"))
+
+for i, item in enumerate(manifest):
+    data = requests.get(item["csv"]).content
+    data += requests.get(item["font"]).content
+    data += requests.get(item["encoding"]).content
+    checksum = binascii.crc32(data)
+    if item["version"] != checksum:
+        manifest[i]["version"] = checksum
+
+file.write_text(json.dumps(manifest, indent=2, ensure_ascii=False), encoding="utf-8")
