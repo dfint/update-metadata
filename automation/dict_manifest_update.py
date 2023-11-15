bb1d0f8390f16347b082855e5c9c86730fe36561
shevernitskiy <mahahuha@ya.ru>
2023-11-15 08:33:40 +0300

    add try catch

--- automation/dict_manifest_update.py
+++ automation/dict_manifest_update.py
@@ -9,11 +9,15 @@ file = Path("./metadata/dict.json")
 manifest = json.loads(file.read_text(encoding="utf-8"))
 
 for i, item in enumerate(manifest):
-    data = requests.get(item["csv"]).content
-    data += requests.get(item["font"]).content
-    data += requests.get(item["encoding"]).content
-    checksum = binascii.crc32(data)
-    if item["version"] != checksum:
-        manifest[i]["version"] = checksum
+    try:
+        data = requests.get(item["csv"]).content
+        data += requests.get(item["font"]).content
+        data += requests.get(item["encoding"]).content
+        checksum = binascii.crc32(data)
+    except:
+        print(f"Fialed on recalculation {item['language']}")
+    else:
+        if item["version"] != checksum:
+            manifest[i]["version"] = checksum
 
 file.write_text(json.dumps(manifest, indent=2, ensure_ascii=False), encoding="utf-8")
