aa0b62afc5a3aa45d3fa1e181cf38a2d562aabd4
insolor <2442833+insolor@users.noreply.github.com>
2024-12-15 22:03:59 +0300

    Don't update hook.json (installer <0.2)

--- automation/hook_manifest_add.py
+++ automation/hook_manifest_add.py
@@ -15,8 +15,7 @@ from utils import get_from_url
 
 base_dir = Path(__file__).parent.parent  # base directory of the repository
 
-hook_json_path = base_dir / "metadata/hook.json"
-hook_v2_json_path = base_dir / "metadata/hook_v2.json"
+hook_json_path = base_dir / "metadata/hook_v2.json"
 offsets_toml_path = base_dir / "store/offsets"
 config_path = base_dir / "store"
 
@@ -32,21 +31,17 @@ class ConfigItem(NamedTuple):
     hook_lib_url: str
     config_url: str
     offsets_url: str
-    dfhooks_url: str | None
+    dfhooks_url: str
     
     def dict(self) -> dict[str, Any]:
-        item = {
+        return {
             "df": self.df_checksum,
             "checksum": self.payload_checksum,
             "lib": self.hook_lib_url,
             "config": self.config_url,
             "offsets": self.offsets_url,
+            "dfhooks": self.dfhooks_url,
         }
-
-        if self.dfhooks_url:
-            item["dfhooks"] = self.dfhooks_url
-
-        return item
 
 
 def update_or_append(manifest_path: str, config_item: ConfigItem):
@@ -70,26 +65,13 @@ def main(hook_lib_url: str, config_file_name: str, offsets_file_name: str, dfhoo
 
     offsets_data = toml.loads(res_offsets.decode(encoding="utf-8"))
     df_checksum = offsets_data["metadata"]["checksum"]
-    payload_checksum = crc32(res_hook + res_config + res_offsets)
-    payload_checksum_v2 = crc32(res_hook + res_config + res_offsets + res_dfhooks)
-    
+    payload_checksum = crc32(res_hook + res_config + res_offsets + res_dfhooks)
+
     update_or_append(
         hook_json_path,
         ConfigItem(
             df_checksum,
             payload_checksum,
-            hook_lib_url,
-            config_base_url + config_file_name,
-            offsets_base_url + offsets_file_name,
-            None,
-        )
-    )
-
-    update_or_append(
-        hook_v2_json_path,
-        ConfigItem(
-            df_checksum,
-            payload_checksum_v2,
             hook_lib_url,
             config_base_url + config_file_name,
             offsets_base_url + offsets_file_name,
--- automation/hook_manifest_update.py
+++ automation/hook_manifest_update.py
@@ -13,27 +13,23 @@ from utils import get_from_url
 
 def main():
     base_dir = Path(__file__).parent.parent  # base directory of the repository
-    hook_json_path = [
-        base_dir / "metadata/hook.json",
-        base_dir / "metadata/hook_v2.json",
-    ]
+    path = base_dir / "metadata/hook_v2.json"
 
-    for path in hook_json_path:
-        manifest = json.loads(path.read_text(encoding="utf-8"))
+    manifest = json.loads(path.read_text(encoding="utf-8"))
 
-        for item in manifest:
-            print(f"Recalculating checksum for {item["df"]=}...")
-            try:
-                data = get_from_url(item["lib"]) + get_from_url(item["config"]) + get_from_url(item["offsets"])
-                if item.get("dfhooks"):
-                    data += get_from_url(item["dfhooks"])
-                checksum = binascii.crc32(data)
-                item["checksum"] = checksum
-            except Exception as ex:
-                print(f"Failed on recalculation {item["df"]=}")
-                raise
+    for item in manifest:
+        print(f"Recalculating checksum for {item["df"]=}...")
+        try:
+            data = get_from_url(item["lib"]) + get_from_url(item["config"]) + get_from_url(item["offsets"])
+            if item.get("dfhooks"):
+                data += get_from_url(item["dfhooks"])
+            checksum = binascii.crc32(data)
+            item["checksum"] = checksum
+        except Exception as ex:
+            print(f"Failed on recalculation {item["df"]=}")
+            raise
 
-        path.write_text(json.dumps(manifest, indent=2, ensure_ascii=False), encoding="utf-8")
+    path.write_text(json.dumps(manifest, indent=2, ensure_ascii=False), encoding="utf-8")
 
 
 if __name__ == "__main__":
